beba2ba28e3c561a86210892f41eeed98ee588c0
Kacy Trujillo <kclnto@gmail.com>
2020-09-15 19:16:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,17 @@ class TicTacToe
   end
 
 
-  def turn
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    player = current_player(board)
+    if (valid_move?(board, index) == true)
+      move(board, index, player)
+      display_board(board)
+    else
+      turn(board)
+    end
   end
 
 
