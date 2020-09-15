77f9336a323c34f325f051bb6533612a6db6fbc6
Kacy Trujillo <kclnto@gmail.com>
2020-09-15 19:25:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,7 @@ class TicTacToe
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    token = current_player(board)
+    token = current_player
     if (valid_move?(index) == true)
       move(index, token)
       display_board
